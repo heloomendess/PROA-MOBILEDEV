ab32556134b3f533f5a63440d33b0d1a314ed25c
Heloisa Mendes <108590744+heloomendess@users.noreply.github.com>
2024-08-19 10:21:36 -0300

    1 a 10 em ordem decrescente em Kotlin
    3. Escreva um algoritmo para imprimir os números de 1 (inclusive) a 10 (inclusive) em ordem decrescente.
    Exemplo: 10, 9, 8, 7, 6, 5, 4, 3, 2, 1

---
+++ horaDeCodar3Ex3.kt
@@ -0,0 +1,9 @@
+/*
+3. Escreva um algoritmo para imprimir os números de 1 (inclusive) a 10 (inclusive) em ordem decrescente.
+Exemplo: 10, 9, 8, 7, 6, 5, 4, 3, 2, 1
+ */
+fun main() {
+    for (numero in 10 downTo 1) {
+        println(numero)
+    }
+}
